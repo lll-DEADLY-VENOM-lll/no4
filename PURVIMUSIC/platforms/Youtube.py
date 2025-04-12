d6b750df8232761ef7ad0753f47fd4b5f77a49e8
DeepVikky88 <radheymusic51@gmail.com>
2025-04-12 08:52:13 +0530

    Update Youtube.py

--- PURVIMUSIC/platforms/Youtube.py
+++ PURVIMUSIC/platforms/Youtube.py
@@ -15,9 +15,9 @@ from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 from youtubesearchpython.__future__ import VideosSearch, CustomSearch
 
-from AnonXMusic import LOGGER
-from AnonXMusic.utils.database import is_on_off
-from AnonXMusic.utils.formatters import time_to_seconds
+from PURVIMUSIC import LOGGER
+from PURVIMUSIC.utils.database import is_on_off
+from PURVIMUSIC.utils.formatters import time_to_seconds
 from config import YT_API_KEY, YTPROXY_URL as YTPROXY
 
 
