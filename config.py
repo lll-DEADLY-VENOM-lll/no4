ddd4ad219816d6d421202ad9d766535e4b510b32
DeepVikky88 <radheymusic51@gmail.com>
2025-05-11 10:47:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 #---------------------------------------------------------------
 
 # Vars For API End Pont.
-YTPROXY_URL = getenv("YTPROXY_URL", 'https://tubed2.okflix.top') ## E.G https://yt.okflix.
+YTPROXY_URL = getenv("YTPROXY_URL", 'https://tubed1.okflix.top') ## E.G https://yt.okflix.
 YT_API_KEY = "freekey15052025"
 #---------------------------------------------------------------
 
