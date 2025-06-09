3cc6ef2fa08cb8e9041bea0bf8e4ecbcfc5754cc
DeepVikky88 <radheymusic51@gmail.com>
2025-06-09 23:10:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,8 +28,7 @@ ASSUSERNAME = getenv("ASSUSERNAME")
 #---------------------------------------------------------------
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 #---------------------------------------------------------------
-API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", "30DxNexGenBots996c27")
+
 #---------------------------------------------------------------
 
 # ----------------------------------------------------------------
