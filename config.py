068a1be64c877d8bf2afdcb1126d857f7f7af234
DeepVikky88 <radheymusic51@gmail.com>
2025-07-25 21:26:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ ASSUSERNAME = getenv("ASSUSERNAME")
 # ---------------------------------------------------------
 # Vars For API End Pont.
 YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## E.G https://yt.okflix.
-YT_API_KEY = getenv("YT_API_KEY" , "xbit_BHNV3FZ1MIMUSQADIBVQ7G" ) ## get your secret key from Deep
+YT_API_KEY = getenv("YT_API_KEY" , "xbit_EF0V2RDJG36GAWO9E1A7TB" ) ## get your secret key from Deep
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
