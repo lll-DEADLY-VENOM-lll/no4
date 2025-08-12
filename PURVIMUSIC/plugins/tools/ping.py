cc16e24ff9f53161bf4e5dab8fc784f0ddab807b
TANYA SINGH <tanyasingh2feb2009@gmail.com>
2025-08-12 10:45:15 +0530

    Update ping.py

--- PURVIMUSIC/plugins/tools/ping.py
+++ PURVIMUSIC/plugins/tools/ping.py
@@ -17,7 +17,7 @@ from config import BANNED_USERS
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_photo(
-        photo="https://telegra.ph/file/c8183c9f6573f7eb799d2.jpg",
+        photo="https://files.catbox.moe/4stxch.jpg",
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await PURVI.ping()
